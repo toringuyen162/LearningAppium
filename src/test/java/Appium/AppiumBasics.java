6c380150de7b559be684933062662c2bf8f63272
toringuyen162 <toringuyen.work@gmail.com>
2023-07-25 17:11:27 +0700

    1

--- src/test/java/Appium/AppiumBasics.java
+++ src/test/java/Appium/AppiumBasics.java
@@ -1,22 +1,37 @@
 package Appium;
-
-import com.sun.security.auth.login.ConfigFile;
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
-import org.testng.annotations.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
 
 public class AppiumBasics extends BaseTest{
+
     @Test
     public void WifiSettingName() throws MalformedURLException {
+        //Locator: xpath,
         driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-        //setwifi name
+        //xpath syntax : tagName[@attribute='value'] -> //tagName
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+        driver.findElement(By.id("android:id/checkbox")).click();
+        driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click(); //Second matching item [2]
+        String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+        Assert.assertEquals(alertTitle,"WiFi settings");
+        driver.findElement(By.id("android:id/edit")).sendKeys("toriwifi");
+        driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
+
+
     }
 
 }
--- src/test/java/Appium/BaseTest.java
+++ src/test/java/Appium/BaseTest.java
@@ -4,24 +4,29 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 public class BaseTest {
     public AndroidDriver driver;
+
     public AppiumDriverLocalService service;
+
     @BeforeClass
     public void ConfigureAppium() throws MalformedURLException {
+
         service = new AppiumServiceBuilder()
                 .withAppiumJS(new File("C:\\Users\\ThuyNV\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
                 .withIPAddress("127.0.0.1")
                 .usingPort(4723)
                 .build();
-
         service.start();
 
         //code
@@ -30,6 +35,7 @@ public class BaseTest {
         options.setApp("D:\\Github\\LearningAppium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//Wait 10 seconds for the elements to be visible
     }
 
     @AfterClass
